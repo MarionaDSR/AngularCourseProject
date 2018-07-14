b5241f01fb80c389cf5b015590dc5e7e0809ff88
Mariona DSR <mariona.nadal@dsrroma.es>
2018-07-14 11:36:25 +0200

    adding ingredients

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,7 +6,7 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-	loadedFeature: string = "recipes";
+	loadedFeature: string = "shopping-list";//"recipes";
 
 	onNavigate(feature: string) {
 		this.loadedFeature = feature;
--- src/app/shopping-list/shopping-edit/shopping-edit.component.ts
+++ src/app/shopping-list/shopping-edit/shopping-edit.component.ts
@@ -1,4 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, EventEmitter, Output} from '@angular/core';
+
+import { Ingredient } from '../../shared/ingredient.model';
 
 @Component({
   selector: 'app-shopping-edit',
@@ -6,10 +8,15 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./shopping-edit.component.css']
 })
 export class ShoppingEditComponent implements OnInit {
+	@Output() addedIngredient = new EventEmitter<Ingredient>();
 
   constructor() { }
 
   ngOnInit() {
+  }
+
+  onAdd(name: string, amount: number) {
+  	this.addedIngredient.emit(new Ingredient(name, amount);
   }
 
 }
