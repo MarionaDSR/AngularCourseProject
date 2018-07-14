9fda8ebe4f6219a520759ea9ecc96e3bc8a0f2ad
Mariona DSR <mariona.nadal@dsrroma.es>
2018-07-14 10:42:55 +0200

    passin recipe data with property binding

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -8,8 +8,8 @@ import { Recipe } from '../recipe.model';
 })
 export class RecipeListComponent implements OnInit {
 	recipes: Recipe[] = [
-		new Recipe('A Test Recipe', 'This is simply a test', 'https://upload.wikimedia.org/wikipedia/commons/c/cc/Recipe-575434.svg'),
-		new Recipe('A Test Recipe', 'This is simply a test', 'https://upload.wikimedia.org/wikipedia/commons/c/cc/Recipe-575434.svg')
+		new Recipe('A Test Recipe 1', 'This is simply a test 1', 'https://upload.wikimedia.org/wikipedia/commons/c/cc/Recipe-575434.svg'),
+		new Recipe('A Test Recipe 2', 'This is simply a test 2', 'https://upload.wikimedia.org/wikipedia/commons/c/cc/Recipe-575434.svg')
 	];
 
   constructor() { }
