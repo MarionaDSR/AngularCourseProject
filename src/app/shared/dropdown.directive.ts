74d3398a424918347892300de08bd5baad34e3af
Mariona DSR <mariona.nadal@dsrroma.es>
2018-07-16 19:57:28 +0200

    dropdown directive (instructor approach)

--- src/app/shared/dropdown.directive.ts
+++ src/app/shared/dropdown.directive.ts
@@ -1,23 +1,15 @@
-import { Directive, ElementRef, Renderer2, HostListener } from '@angular/core';
+import { Directive, ElementRef, Renderer2, HostListener, HostBinding } from '@angular/core';
 
 @Directive({
   selector: '[appDropdown]'
 })
 export class DropdownDirective {
-	open: boolean = false;
-
-  constructor(private elementRef: ElementRef,
-  	private renderer: Renderer2) { }
+	@HostBinding('class.open') open: boolean = false;
 
   // build a dropdirective that attaches 'open' class on first click and remove it on second click.
 
   @HostListener('click') appDropdown() {
   	this.open = !this.open; 
-  	if (this.open) {
-  		this.renderer.addClass(this.elementRef.nativeElement, 'open');
-  	} else {
-  		this.renderer.removeClass(this.elementRef.nativeElement, 'open');  	
-  	}
   }
 
 }
