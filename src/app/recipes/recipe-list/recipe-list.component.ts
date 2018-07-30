77ba5f814156076550be209a482e2c4c848cd171
Mariona DSR <mariona.nadal@dsrroma.es>
2018-07-30 20:08:12 +0200

    retreiving route parameters(instructor approach)

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,6 +1,7 @@
 import { RecipesService } from './../recipes.service';
 import { Component, OnInit } from '@angular/core';
 import { Recipe } from '../recipe.model';
+import { Router, ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-recipe-list',
@@ -11,9 +12,15 @@ export class RecipeListComponent implements OnInit {
 
    recipes: Recipe[];
 
-  constructor(private recipesService: RecipesService) { }
+  constructor(private recipesService: RecipesService,
+    private router: Router,
+    private route: ActivatedRoute) { }
 
   ngOnInit() {
     this.recipes = this.recipesService.getRecipes();
+  }
+
+  onNewRecipe() {
+    this.router.navigate(['new'],  {relativeTo: this.route});
   }
 }
